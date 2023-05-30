e7138a27383541b48fcb70f53a904e856c5b03b3
Titouan Hirsch <titouanhirsch@gmail.com>
2023-05-30 08:27:22 +0200

    routes and toothbrush controller done

--- app/controllers/toothbrush_controller.rb
+++ app/controllers/toothbrush_controller.rb
@@ -1,22 +1,44 @@
 class ToothbrushController < ApplicationController
   def index
+    @toothbrushs = Toothbrush.all
   end
 
   def show
+    # @review = Review.new
+    @toothbrush = Toothbrush.find(params[:id])
   end
 
   def new
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    @toothbrush = Toothbrush.new
   end
 
   def create
+    @toothbrush = Toothbrush.new(toothbrush_params)
+    if @toothbrush.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @toothbrush = Toothbrush.find(params[:id])
+  end
+
+  def update
+    @toothbrush = Toothbrush.find(params[:id])
+    @toothbrush.update(params[:toothbrush])
+  end
+
+  def destroy
+    @toothbrush = Toothbrush.find(params[:id])
+    @toothbrush.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def toothbrush_params
+    params.require(:toothbrush).permit(:category, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'booking/new'
-  get 'toothbrush/index'
-  get 'toothbrush/show'
-  get 'toothbrush/new'
-  get 'toothbrush/edit'
-  get 'toothbrush/update'
-  get 'toothbrush/destroy'
-  get 'toothbrush/create'
+  resources :toothbrushes do
+    resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:create]
+  end
   devise_for :users
   root to: "pages#home"
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
